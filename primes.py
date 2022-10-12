0991ea4ef07544cab2675ea23054eb3a7c24f4a0
Bowen Zhu <bowen.zhu@kcl.ac.uk>
2022-10-12 21:22:08 +0100

    First Try

--- primes.py
+++ primes.py
@@ -2,5 +2,17 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes < 0:
+        raise ValueError (f"should have been a positive number")
+    elif not number_of_primes:
+        raise ValueError (f"should give a value")
+    number = 1
     list = []
+    while len(list) < number_of_primes:
+        for k in range(1,number+1):
+            if number == 1 or (k != 1 and k != number and number % k == 0):
+                break
+            elif k == number:
+                list.append(number)
+        number+=1
     return list
